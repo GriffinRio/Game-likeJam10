324eae6e861324d10a397f1fd4a718077196f113
GriffinRio <griffinator03@gmail.com>
2026-07-24 21:19:21 -0700

    changed how break time is calculated

--- gameLikeJam10/Resources/Templates/block.gd
+++ gameLikeJam10/Resources/Templates/block.gd
@@ -3,6 +3,7 @@ class_name Block
 
 @export var name : String
 @export var tool_type : Enums.tool_type
-@export var break_time : float
+@export var hardness : float
+@export var drop_tools : Array[Item]
 ## Connects to an Item that will be giving to the player when block is broken
 @export var drop : Item
--- gameLikeJam10/main.gd
+++ gameLikeJam10/main.gd
@@ -19,13 +19,18 @@ func _process(delta: float) -> void:
 func _on_player_start_mining(tile_position: Vector2i, equipped: Item) -> void:
 	var block : Block = tile_map.get_tile(tile_position)
 	# TODO: System for tools speeding up block breaking depending on type.
-	var multiplier : float = 1 / block.break_time
+	var break_time : float = block.hardness
 	if(equipped is ItemTools):
-		var tool : ItemTools  = equipped
+		var tool : ItemTools = equipped
+		if(block.drop_tools.size() == 0 or tool in block.drop_tools):
+			break_time *= 1.5
+		else:
+			break_time *= 5.0
 		if(tool.tool_type == block.tool_type):
-			multiplier *= tool.multiplier
+			break_time /= tool.multiplier
+	print(break_time)
 	breaking.update_position(tile_position)
-	breaking.begin_break(multiplier)
+	breaking.begin_break(break_time)
 
 func _on_player_stop_mining() -> void:
 	breaking.reset_break()
