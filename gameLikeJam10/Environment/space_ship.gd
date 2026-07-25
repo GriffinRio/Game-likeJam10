002e2f1179b23795f3ab85554d6c6cdf174ee6f1
GriffinRio <griffinator03@gmail.com>
2026-07-25 14:03:40 -0700

    added spaceship

---
+++ gameLikeJam10/Environment/space_ship.gd
@@ -0,0 +1,21 @@
+extends Node2D
+class_name SpaceShip
+
+@onready var interact_text: Control = $Interact_Text
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	interact_text.visible = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	interact_text.visible = true
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	interact_text.visible = false
+
+func interact() -> void:
+	print("crafting...")
