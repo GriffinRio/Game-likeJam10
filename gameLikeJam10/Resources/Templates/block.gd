0414a2867d1a1cd6c3710452f8c89ddbfe44652a
GriffinRio <griffinator03@gmail.com>
2026-07-25 14:04:09 -0700

    small changes to layering and properties.

--- gameLikeJam10/Resources/Templates/block.gd
+++ gameLikeJam10/Resources/Templates/block.gd
@@ -4,6 +4,6 @@ class_name Block
 @export var name : String
 @export var tool_type : Enums.tool_type
 @export var hardness : float
-@export var drop_tools : Array[Item]
+@export var drop_tools : Array[ItemTools]
 ## Connects to an Item that will be giving to the player when block is broken
 @export var drop : Item
--- gameLikeJam10/Resources/Templates/item_tools.gd
+++ gameLikeJam10/Resources/Templates/item_tools.gd
@@ -3,4 +3,3 @@ class_name ItemTools
 
 @export var tool_type : Enums.tool_type
 @export var multiplier : float = 1.0
-@export var recipe : Array[Item]
