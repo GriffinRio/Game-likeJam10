d0651dac0bfc8af2c303f71cc07e8b03cf4109f9
GriffinRio <griffinator03@gmail.com>
2026-07-29 14:14:50 -0700

    fixed issue with count text updating all instances

--- gameLikeJam10/UI/crafting_slot_item.gd
+++ gameLikeJam10/UI/crafting_slot_item.gd
@@ -10,16 +10,16 @@ var goal : Item
 func _ready() -> void:
 	texture_rect.texture = goal.sprite
 	count.text = "0/" + str(goal.count)
-	count.label_settings.font_color = Color.RED
+	count.add_theme_color_override("font_color", Color.RED)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func update_count(item: Item) -> void:
-	count.text = str(item.count) + "/" + str(goal.count)
+func update_count(new_count: int) -> void:
+	count.text = str(new_count) + "/" + str(goal.count)
 	#TODO: Changes color of all item descriptions, not just this one :(, use theme overides and not label settings?
-	if(item.count < goal.count):
-		count.label_settings.font_color = Color.RED
+	if(new_count < goal.count):
+		count.add_theme_color_override("font_color", Color.RED)
 	else:
-		count.label_settings.font_color = Color.WHITE
+		count.add_theme_color_override("font_color", Color.WHITE)
