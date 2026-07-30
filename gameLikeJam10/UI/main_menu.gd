554f0141f93a903611f513bcc22505b6977fcac2
GriffinRio <griffinator03@gmail.com>
2026-07-30 13:42:11 -0700

    Added very basic controls menu

--- gameLikeJam10/UI/main_menu.gd
+++ gameLikeJam10/UI/main_menu.gd
@@ -1,17 +1,19 @@
 extends Control
 
 const MAIN = preload("uid://8mqlsnp7uqw4")
+@onready var controls: Control = $Controls
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	controls.visible = false
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_button_pressed() -> void:
+func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_packed(MAIN)
+
+
+func _on_controls_pressed() -> void:
+	controls.visible = true
+
+func _on_exit_controls_pressed() -> void:
+	controls.visible = false
